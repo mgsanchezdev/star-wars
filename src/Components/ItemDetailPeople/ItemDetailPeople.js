c9088f8695a22abb819724e3ddd2dc09c6e22740
Mario Sanchez <mgsanchezdev@gmail.com>
2021-04-02 13:34:25 -0300

    Ajuste de estilos en diferentes componentes

--- src/Components/ItemDetailPeople/ItemDetailPeople.js
+++ src/Components/ItemDetailPeople/ItemDetailPeople.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { PeopleDetailsContext } from '../../context/peopleDetails';
+import './itemDetailPeople.css';
 
 const ItemDetailPeople = () => {
   const { people } = React.useContext(PeopleDetailsContext);
   return (
-    <div>
+    <div className="container-detail-people">
       <h1>Nombre: {people?.name}</h1>
       <h2>Altura: {people?.height}</h2>
-      <h2>Color de Ojos: {people?.eye_color}</h2>
+      <h2>Color de ojos: {people?.eye_color}</h2>
       <h2>Fecha nacimiento: {people?.birth_year}</h2>
       <h2>Género: {people?.gender}</h2>
       <h2>Color de pelo: {people?.hair_color}</h2>
--- src/Components/Layout/Header/Header.js
+++ src/Components/Layout/Header/Header.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Image, Button } from 'react-bootstrap';
+import { Image } from 'react-bootstrap';
 import logo from '../../../assets/images/star-wars.jpg';
 import './header.css';
 
 const Header = () => (
   <div className="container-image">
     <Image className="image-header" src={logo} />
-    <Link to="/">
-      <Button className="btn-home">Home</Button>
+    <Link to="/" className="btn-home">
+      Home{' '}
     </Link>
   </div>
 );
