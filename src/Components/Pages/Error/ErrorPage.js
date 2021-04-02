9e265a3470b2ea8535d314e449e19a3368ea6182
Mario Sanchez <mgsanchezdev@gmail.com>
2021-04-02 15:06:19 -0300

    Logica y estilos de errorPage

--- src/Components/Pages/Error/ErrorPage.js
+++ src/Components/Pages/Error/ErrorPage.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import './errorPage.css';
 
-const ErrorPage = () => <div>Error 404</div>;
+const ErrorPage = () => (
+  <div className="error">
+    <h1>404</h1>
+    <p>No encontramos lo que buscabas.</p>
+  </div>
+);
 
 export default ErrorPage;
